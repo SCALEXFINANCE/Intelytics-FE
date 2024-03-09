49b1d42257910ecb860252b9f7791ad19353256a
tany071 <tanyasinha7u@gmail.com>
2024-03-09 21:41:47 +0530

    :sync: fix token table conflict

--- src/components/TokensTable.tsx
+++ src/components/TokensTable.tsx
@@ -221,6 +221,7 @@ export function TokensTable() {
 
         // Format the amount as a dollar amount
         const formatted = new Intl.NumberFormat("en-US", {
+       
           currency: "USD",
         }).format(amount);
 
@@ -288,7 +289,7 @@ export function TokensTable() {
         );
         const liquidityData3 = apiUrl3.data;
         const alienLiq = liquidityData3.pairs[0].liquidity.usd;
-        const alienPrice = liquidityData3.pairs[0].priceUsd;
+        const alienPrice = response2.data.coins["coingecko:alien"].price;
 
         // kira
         const response3 = await axios.get(
@@ -311,7 +312,7 @@ export function TokensTable() {
         );
         const liquidityData = apiUrl.data;
         const dojoLiq = liquidityData.pairs[0].liquidity.usd;
-        const dojoPrice = liquidityData.pairs[0].priceUsd;
+        const dojoPrice = response4.data.coins["coingecko:dojo-token"].price;
 
         // katana
         const response5 = await axios.get(
@@ -420,10 +421,10 @@ export function TokensTable() {
             category: "Utility ",
             Price: dojoPrice,
             Liquidity: dojoLiq,
-            "Market Cap": 20000000 * dojoPrice,
-            "Circulating Supply": 20000000,
-            "Total Supply": 800000000,
-            FDV: 800000000 * dojoPrice,
+            "Market Cap": 200000000 * dojoPrice,
+            "Circulating Supply": 200000000,
+            "Total Supply": 80000000,
+            FDV: 80000000 * dojoPrice,
           },
           {
             name: "Dinj",
