782fe2bf6a67ee610640a3d4557e44f034a84b96
tany071 <tanyasinha7u@gmail.com>
2024-03-14 20:54:23 +0530

    sync with main

--- src/pages/Tokens/content.ts
+++ src/pages/Tokens/content.ts
@@ -2,48 +2,60 @@ export const contents: any = {
   dojo: {
     name: "Dojo",
     category: "Utility",
-    apiUrl: "https://api.dexscreener.com/latest/dex/tokens/inj1zdj9kqnknztl2xclm5ssv25yre09f8908d4923"
+    apiUrl:
+      "https://api.dexscreener.com/latest/dex/tokens/inj1zdj9kqnknztl2xclm5ssv25yre09f8908d4923",
   },
 
   kira: {
     name: "Kira",
     category: "Meme",
-    apiUrl:"https://api.dexscreener.com/latest/dex/tokens/factory-inj1xy3kvlr4q4wdd6lrelsrw2fk2ged0any44hhwq-KIRA"
+    apiUrl:
+      "https://api.dexscreener.com/latest/dex/tokens/factory-inj1xy3kvlr4q4wdd6lrelsrw2fk2ged0any44hhwq-KIRA",
   },
 
   alien: {
     name: "Alien",
     category: "Utility",
-    apiUrl:"https://api.dexscreener.com/latest/dex/tokens/factory-inj1mly2ykhf6f9tdj58pvndjf4q8dzdl4myjqm9t6-ALIEN"
+    apiUrl:
+      "https://api.dexscreener.com/latest/dex/tokens/factory-inj1mly2ykhf6f9tdj58pvndjf4q8dzdl4myjqm9t6-ALIEN",
   },
 
   ninja: {
     name: "Ninja",
     category: "Meme",
-    apiUrl:"https://api.dexscreener.com/latest/dex/tokens/factory-inj1xtel2knkt8hmc9dnzpjz6kdmacgcfmlv5f308w-ninja"
+    apiUrl:
+      "https://api.dexscreener.com/latest/dex/tokens/factory-inj1xtel2knkt8hmc9dnzpjz6kdmacgcfmlv5f308w-ninja",
   },
 
   stinj: {
     name: "Stinj",
     category: "Utility",
-    apiUrl: "https://api.dexscreener.com/latest/dex/tokens/ibc-AC87717EA002B0123B10A05063E69BCA274BA2C44D842AEEB41558D2856DCE93"
+    apiUrl:
+      "https://api.dexscreener.com/latest/dex/tokens/ibc-AC87717EA002B0123B10A05063E69BCA274BA2C44D842AEEB41558D2856DCE93",
   },
 
   dinj: {
     name: "Dinj",
     category: "Utility ",
-    apiUrl:"https://api.dexscreener.com/latest/dex/tokens/inj134wfjutywny9qnyux2xgdmm0hfj7mwpl39r3r9"
+    apiUrl:
+      "https://api.dexscreener.com/latest/dex/tokens/inj134wfjutywny9qnyux2xgdmm0hfj7mwpl39r3r9",
   },
 
   zignaly: {
     name: "Zignaly",
     category: "Utility",
-    apiUrl:"https://api.dexscreener.com/latest/dex/tokens/peggy0xb2617246d0c6c0087f18703d576831899ca94f01"
+    apiUrl:
+      "https://api.dexscreener.com/latest/dex/tokens/peggy0xb2617246d0c6c0087f18703d576831899ca94f01",
   },
 
-  roll:{
-    name:"Roll",
-    category:"Gaming",
-    apiUrl:"https://api.dexscreener.com/latest/dex/tokens/inj1qv98cmfdaj5f382a0klq7ps4mnjp6calzh20h3"
-  }
+  roll: {
+    name: "Roll",
+    category: "Gaming",
+    apiUrl:
+      "https://api.dexscreener.com/latest/dex/tokens/inj1qv98cmfdaj5f382a0klq7ps4mnjp6calzh20h3",
+  },
+  babydojo: {
+    name: "BabyDOJO",
+    category: "CW-20",
+  },
 };
