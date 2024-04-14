dcf7586badc5375603f1f2c25837caf29643cb0e
Utkarsh1446 <100139597+Utkarsh1446@users.noreply.github.com>
2024-04-15 00:55:31 +0530

    Token

--- src/components/DojoChart.tsx
+++ src/components/DojoChart.tsx
@@ -56,7 +56,6 @@
 
 // export default memo(DojoChart);
 
-
 import React, { useEffect, useState } from "react";
 import {
   Chart as ChartJs,
@@ -101,7 +100,7 @@ const DojoChart = ({ height, width }: any) => {
       console.log(protocols.chainTvls.Injective.tvl);
       const tvl = protocols.chainTvls.Injective.tvl;
       const values: any = [];
-      tvl.forEach((data:any) => {
+      tvl.forEach((data: any) => {
         values.push(data.totalLiquidityUSD);
       });
       console.log(values);
@@ -174,16 +173,25 @@ const DojoChart = ({ height, width }: any) => {
         display: true,
       },
     },
+    scales: {
+      y: {
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value: any) {
+            return "$" + value / 1000000 + "M";
+          },
+        },
+      },
+    },
   };
   useEffect(() => {
     chartValues();
   }, []);
   return (
-    <div className=" shadow-xl w-[50vw] h-[50vh] p-4 rounded-xl">
+    <div className=" shadow-xl lg:w-[50vw]  lg:h-[50vh] w-[84vw] lg:p-4  rounded-xl">
       <Line data={data} options={options} width={width} height={height} />
     </div>
   );
 };
 
 export default DojoChart;
-
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout><Component {...pageProps} /></Layout>;
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
+  );
 }
