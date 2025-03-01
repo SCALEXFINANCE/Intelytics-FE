8e661ce49fd835e1e9620c9a9132237a5c0ca068
code-bajju <11212766@mmec.mmdu.org>
2025-03-01 10:15:59 +0530

    uPDATED TOKEN

--- src/pages/tokens/[token].tsx
+++ src/pages/tokens/[token].tsx
@@ -1,10 +1,11 @@
 import { useRouter } from "next/router";
-import { Key, useEffect, useState } from "react";
+import { Key, useEffect, useState, useRef } from "react";
 import Image from "next/image";
 import TokenChart from "@/components/tokenChart";
 import bannerdefault from "@/components/assets/banner-default.png";
 import tokendefault from "@/components/assets/404-image.png";
 import Swap from "@/components/swap/Swap";
+
 const API_URL = process.env.NEXT_PUBLIC_DEXSCREENER_API;
 
 const TokenDetails = () => {
@@ -15,10 +16,10 @@ const TokenDetails = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [copied, setCopied] = useState(false);
 
-
+  const scriptLoaded = useRef(false); // Track script execution
 
   useEffect(() => {
-    if (!token) return;
+    if (!token || scriptLoaded.current) return;
 
     const fetchTokenDetails = async () => {
       try {
@@ -33,15 +34,14 @@ const TokenDetails = () => {
     };
 
     fetchTokenDetails();
+    scriptLoaded.current = true; // Mark script as loaded
   }, [token]);
 
   if (loading) return <p className="text-center">Loading...</p>;
-  if (!tokenData.length) return <p className="text-center">Token not found.</p>;
+  if (!tokenData?.length) return <p className="text-center">Token not found.</p>;
 
   return (
     <div className="p-4 min-h-screen">
-      {/* Header Banner */}
-
       {tokenData.map(
         (
           pair: {
@@ -57,13 +57,12 @@ const TokenDetails = () => {
             pairAddress: any;
             priceUsd: any;
             liquidity: { usd: any };
-
             volume: { h24: any; h6: any; h1: any };
             priceChange: { h24: any };
           },
           index: Key | null | undefined
         ) => (
-          <div key={index} className="">
+          <div key={index}>
             <div className="lg:flex text-white text-center rounded-lg shadow-md border border-bordercolor p-2 lg:p-4">
               <div className="w-full flex justify-center items-center">
                 <Image
@@ -110,98 +109,89 @@ const TokenDetails = () => {
                     ${pair.priceChange?.h24 || "N/A"}
                   </div>
                 </div>
-<div className="lg:px-2">
-<div
-                  className="text-left text-lg font-bold"
-                  style={{ textShadow: "white 2px 2px 20px;" }}
-                >
-                
-                  GENERAL INFO
-                  
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="lg:p-4 p-1 rounded-lg border border-bordercolor text-left">
-                    <p>
-                      <strong>Chain ID:</strong> {pair.chainId || "N/A"}
-                    </p>
-                    <p>
-                      <strong>Dex ID:</strong> {pair.dexId || "N/A"}
-                    </p>
-                    <p className="overflow-hidden">
-                      <strong>Pair Address:</strong> {pair.pairAddress || "N/A"}
-                      <div>
-  <button
-    onClick={() => {
-      navigator.clipboard.writeText(pair.pairAddress).then(() => {
-        setCopied(true);
-        setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
-      });
-    }}
-    className={`rounded ${
-      copied ? " text-red-600" : "text-green-600"
-    }`}
-  >
-    {copied ? "Copied!" : "Copy Address"}
-  </button>
-</div>
-                    </p>
-                  </div>
 
-                  <div className="lg:p-4 p-1 rounded-lg border border-bordercolor text-left">
-                    <p>
-                      <strong>24H Volume:</strong> ${pair.volume?.h24 || "N/A"}
-                    </p>
-                    <p>
-                      <strong>6H Volume:</strong> ${pair.volume?.h6 || "N/A"}
-                    </p>
-                    <p>
-                      <strong>1H Volume:</strong> ${pair.volume?.h1 || "N/A"}
-                    </p>
+                <div className="lg:px-2">
+                  <div
+                    className="text-left text-lg font-bold"
+                    style={{ textShadow: "white 2px 2px 20px" }}
+                  >
+                    GENERAL INFO
                   </div>
+                  <div className="grid grid-cols-2 gap-4">
+                    <div className="lg:p-4 p-1 rounded-lg border border-bordercolor text-left">
+                      <p>
+                        <strong>Chain ID:</strong> {pair.chainId || "N/A"}
+                      </p>
+                      <p>
+                        <strong>Dex ID:</strong> {pair.dexId || "N/A"}
+                      </p>
+                      <p className="overflow-hidden">
+                        <strong>Pair Address:</strong> {pair.pairAddress || "N/A"}
+                        <div>
+                          <button
+                            onClick={() => {
+                              navigator.clipboard.writeText(pair.pairAddress).then(() => {
+                                setCopied(true);
+                                setTimeout(() => setCopied(false), 2000);
+                              });
+                            }}
+                            className={`rounded ${copied ? "text-red-600" : "text-green-600"}`}
+                          >
+                            {copied ? "Copied!" : "Copy Address"}
+                          </button>
+                        </div>
+                      </p>
+                    </div>
 
-                  {/* Social Links */}
-                  <div className="lg:p-4 p-1 rounded-lg border border-bordercolor text-left col-span-2">
-                    <p>
-                      <strong>Website:</strong>{" "}
-                      <a
-                        href={pair.info?.websites?.[0]?.url || "#"}
-                        target="_blank"
-                        className="text-blue-300"
-                      >
-                        {pair.info?.websites?.[0]?.label || "N/A"}
-                      </a>
-                    </p>
-                    <p>
-                      <strong>Socials:</strong>
-                    </p>
-                    <ul className="list-disc pl-4">
-                      {pair.info?.socials?.length ? (
-                        pair.info.socials.map((social, i) => (
-                          <li key={i}>
-                            <a
-                              href={social.url}
-                              target="_blank"
-                              className="text-blue-300"
-                            >
-                              {social.type}
-                            </a>
-                          </li>
-                        ))
-                      ) : (
-                        <li>N/A</li>
-                      )}
-                    </ul>
+                    <div className="lg:p-4 p-1 rounded-lg border border-bordercolor text-left">
+                      <p>
+                        <strong>24H Volume:</strong> ${pair.volume?.h24 || "N/A"}
+                      </p>
+                      <p>
+                        <strong>6H Volume:</strong> ${pair.volume?.h6 || "N/A"}
+                      </p>
+                      <p>
+                        <strong>1H Volume:</strong> ${pair.volume?.h1 || "N/A"}
+                      </p>
+                    </div>
+
+                    {/* Social Links */}
+                    <div className="lg:p-4 p-1 rounded-lg border border-bordercolor text-left col-span-2">
+                      <p>
+                        <strong>Website:</strong>{" "}
+                        <a
+                          href={pair.info?.websites?.[0]?.url || "#"}
+                          target="_blank"
+                          className="text-blue-300"
+                        >
+                          {pair.info?.websites?.[0]?.label || "N/A"}
+                        </a>
+                      </p>
+                      <p>
+                        <strong>Socials:</strong>
+                      </p>
+                      <ul className="list-disc pl-4">
+                        {pair.info?.socials?.length ? (
+                          pair.info.socials.map((social, i) => (
+                            <li key={i}>
+                              <a href={social.url} target="_blank" className="text-blue-300">
+                                {social.type}
+                              </a>
+                            </li>
+                          ))
+                        ) : (
+                          <li>N/A</li>
+                        )}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-</div>
               </div>
             </div>
             <div className="rounded-md my-4 h-full lg:flex w-full">
               <div className="w-full">
                 <TokenChart />
-             
               </div>
-          
             </div>
           </div>
         )
